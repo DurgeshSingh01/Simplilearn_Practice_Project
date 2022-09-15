203d3b86921bacdb5284c15703cca044a429822b
DurgeshSingh01 <95412673+DurgeshSingh01@users.noreply.github.com>
2022-09-15 10:31:08 +0530

    Commit

---
+++ PracticeProject1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("D:\\Mphasis\\Practice Project\\StudentData.txt");
+            Console.WriteLine("Data From the File display here-");
+            Console.WriteLine("=========================================");
+            Console.WriteLine(data);
+            Console.WriteLine("=========================================");
+        }
+    }
+}
