6f442276a81014091c4f6d722ea8e6cfbd571511
Durgesh Singh <95412673+DurgeshSingh01@users.noreply.github.com>
2022-10-16 01:12:52 +0530

    Add files via upload

---
+++ Phase_3_Projects/School_WebAPI_Marks/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_School_Project.Models
+{
+    public class StudentModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int ClassID { get; set; }
+        public string SubjectName { get; set; }
+        public int? Marks { get; set; }
+    }
+}
